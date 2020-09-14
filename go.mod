88499cf2d94b065b8d21a5f1e35afcee925d670a
iclannad <975516776@qq.com>
2020-09-14 07:25:20 +0000

    add websocket support

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module tcp-server
 
 go 1.14
+
+require (
+	github.com/gorilla/websocket v1.4.1
+	github.com/satori/go.uuid v1.2.1-0.20181028125025-b2ce2384e17b
+)
--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"bufio"
 	"encoding/hex"
 	"fmt"
+	"github.com/gorilla/websocket"
+	uuid "github.com/satori/go.uuid"
 	"math/rand"
 	"net"
+	"net/http"
 	"strings"
 	"time"
 )
@@ -21,18 +24,23 @@ func main() {
 	defer l.Close()
 	rand.Seed(time.Now().Unix())
 
+	wbMap := make(map[string]*websocket.Conn)
+
+	// websocket server
+	go httpStart(&wbMap)
+
+	// tcp server
 	for {
 		c, err := l.Accept()
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		go handleConnection(c)
+		go handleConnection(c, &wbMap)
 	}
-
 }
 
-func handleConnection(c net.Conn) {
+func handleConnection(c net.Conn, wbMap *map[string]*websocket.Conn) {
 	fmt.Printf("Serving %s\n", c.RemoteAddr().String())
 
 	for {
@@ -41,12 +49,56 @@ func handleConnection(c net.Conn) {
 		lengthPer, err := bufio.NewReader(c).Read(buf)
 		buf = buf[:lengthPer]
 		temp := strings.TrimSpace(string(buf))
-		fmt.Println("hex==> ", hex.EncodeToString(buf))
-		fmt.Println("string ==>>", temp)
+		fmt.Println("tcp receive ==>> ", time.Now().Format("[2006-01-02 15:04:05]"), " ", "hex==> ", hex.EncodeToString(buf), " ", "string ==>>", temp)
+
+		for _, v := range *wbMap {
+			if v != nil {
+				v.WriteMessage(1, buf)
+			}
+		}
+
 		if err != nil {
 			fmt.Println(err)
 			break
 		}
+
 	}
 	c.Close()
+}
+
+func httpStart(wbMap *map[string]*websocket.Conn) {
+	http.HandleFunc("/websocket", func(w http.ResponseWriter, r *http.Request) {
+		var upgrader = websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+		}
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			http.Error(w, "Upgrade fail", http.StatusBadRequest)
+			return
+		}
+		v4, err := uuid.NewV4()
+		key := v4.String()
+
+		// add websocket conn to list
+		(*wbMap)[key] = conn
+
+		// read data from client
+		for {
+			msgType, data, err := conn.ReadMessage()
+			if err != nil {
+				if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+					conn.Close()
+					delete(*wbMap, key)
+					fmt.Println(err)
+				}
+				break
+			}
+			fmt.Println("websocket receive ==>> ", time.Now().Format("[2006-01-02 15:04:05]"), " ", msgType, strings.TrimSpace(string(data)))
+		}
+
+	})
+
+	http.ListenAndServe(":9002", nil)
 }
